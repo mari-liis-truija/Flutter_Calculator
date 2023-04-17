04e64fca049980b158f59512abe683749ae5165c
Truija M <mariliis.truija@gmail.com>
2023-04-18 02:45:56 +0300

    first commit

---
+++ 01_calculator_ui/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Basic Calculator UI',
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+      ),
+      home: const CalculatorScreen(),
+    );
+  }
+}
